5f8c45c916f5742534a7043744449995761a97ae
Kirimino <brianonduso08@gmail.com>
2024-03-22 20:43:01 +0300

    Complete FunctionsLab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+    const headquarters = 42;
+    if (someValue > headquarters) {
+        return someValue - headquarters;
+    } else {
+        return headquarters - someValue;
+    }
+}
+
+
+function distanceFromHqInFeet(someValue) {
+    const headquarters = 42;
+    const distanceInBlocks = Math.abs(someValue - headquarters);
+    return distanceInBlocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const distanceInBlocks = Math.abs(start - destination);
+    const feetPerBlock = 264;
+    
+    if (start > destination) {
+        return distanceInBlocks * feetPerBlock;
+    } else {
+        return distanceInBlocks * feetPerBlock;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare = 0;
+    if (distance <= 400) {
+        fare = 0;
+    } else if (distance > 400 && distance <= 2000) {
+        fare = (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        fare = 25;
+    } else {
+        return 'cannot travel that far';
+    }
+    return fare;
+}
